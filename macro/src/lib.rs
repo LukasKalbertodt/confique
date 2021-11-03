e5af4f8efe20280d782e941aa6371a9dddc5800c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-11-03 11:46:33 +0100

    Replace some manual parsing code with `darling` helper crate

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -3,14 +3,19 @@ use proc_macro::TokenStream as TokenStream1;
 
 mod gen;
 mod ir;
+mod parse;
 mod util;
 
 
 #[proc_macro_derive(Config, attributes(config))]
 pub fn config(input: TokenStream1) -> TokenStream1 {
-    syn::parse2::<syn::DeriveInput>(input.into())
-        .and_then(ir::Input::from_ast)
+    let input = match syn::parse2::<syn::DeriveInput>(input.into()) {
+        Err(e) => return e.to_compile_error().into(),
+        Ok(i) => i,
+    };
+
+    ir::Input::from_ast(input)
         .map(gen::gen)
-        .unwrap_or_else(|e| e.to_compile_error())
+        .unwrap_or_else(|e| e.write_errors())
         .into()
 }
