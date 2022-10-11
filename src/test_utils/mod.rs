963e4745a3c37f5ab3ba89a92a8bdb5a465c59fc
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-10-11 13:17:19 +0200

    Allow unused test utility
    This is only unused with `--no-default-features`.

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -1,10 +1,12 @@
 pub(crate) mod example1;
 
 
+#[allow(unused_macros)]
 macro_rules! include_format_output {
     ($file:expr) => {
         include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/format-output/", $file))
     };
 }
 
+#[allow(unused_imports)]
 pub(crate) use include_format_output;
